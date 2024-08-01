7fcc469b9971cad2521e4afcf04b2b9534cb6a97
pindjouf <esaubukasa@protonmail.com>
2024-08-02 01:18:17 +0200

    led solution stolen from nandland
    will take the time to totally understand everything tomorrow. I already
    have some notes that I'm working on, but let's not forget the title of
    the previous chapter "cheating our way past the transistor".

---
+++ Section 2: Bringup/led.sv
@@ -0,0 +1,13 @@
+module bink #(parameter g_COUNT_1HZ = 12500000) (input  i_Clk, output reg o_LED = 1'b0);
+
+    reg [31:0] r_Count_1Hz = 0;
+  
+    always @(posedge i_Clk) begin
+        if (r_Count_1Hz == g_COUNT_1HZ) begin
+            o_LED <= ~o_LED;
+            r_Count_1Hz <= 0;
+        end else begin
+            r_Count_1Hz <= r_Count_1Hz + 1;
+        end
+    end
+endmodule // blink
